00c93f31608b603e4a5a76d267df67240004ffbb
Saif <113651806+saifs27@users.noreply.github.com>
2023-11-16 01:39:35 -0600

    handle white pawn moves

--- src/movegen.c
+++ src/movegen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "defs.h"
 
-#define MOVE(from,to,capture,promote,fl) ( (from) | ((to) << 7) | ((capture) << 14) | (promote)<< 20 | (fl))
+#define MOVE(from,to,capture,promote,fl) ( (from) | ((to) << 7) | ((capture) << 14) | (promote) << 20 | (fl))
 #define SQUARE_OFFBOARD(sq) (FilesBoard[(sq)]==OFFBOARD)
 
 
@@ -26,6 +26,37 @@ void add_move_enPassant(const S_BOARD *pos, int move, S_MOVELIST *list) {
 
 }
 
+void add_white_pawn_capture_move(const S_BOARD *pos, const int from , const int to, const int capture, S_MOVELIST *list) {
+    // handle promotion
+    if (RanksBoard[from] == 7) {
+        add_move_capture(pos, MOVE(from, to, capture, wQ, 0), list);
+        add_move_capture(pos, MOVE(from, to, capture, wR, 0), list);
+        add_move_capture(pos, MOVE(from, to, capture, wN, 0), list);
+        add_move_capture(pos, MOVE(from, to, capture, wB, 0), list);
+    }
+    else {
+        add_move_capture(pos, MOVE(from, to, capture, EMPTY, 0), list);
+    }
+}
+
+void add_white_pawn_move(const S_BOARD *pos, const int from , const int to, S_MOVELIST *list) {
+    // handle promotion
+    if (RanksBoard[from] == 7) {
+        add_move_quiet(pos, MOVE(from, to, EMPTY, wQ, 0), list);
+        add_move_quiet(pos, MOVE(from, to, EMPTY, wR, 0), list);
+        add_move_quiet(pos, MOVE(from, to, EMPTY, wN, 0), list);
+        add_move_quiet(pos, MOVE(from, to, EMPTY, wB, 0), list);
+    }
+    else {
+        add_move_quiet(pos, MOVE(from, to, EMPTY, EMPTY, 0), list);
+    }
+}
+
 void generate_all_moves(const S_BOARD *pos, S_MOVELIST *list) {
-    list->count = 0;
+    ASSERT(board_check(pos));
+
+    int piece = EMPTY;
+    int side = pos->side;
+    int sq = 0; int t_sq = 0;
+    int pieceNum = 0;
 }
